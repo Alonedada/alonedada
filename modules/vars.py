5b9b70fe1f772e8806672ac04b8a24b545b80631
Alonedada <prakashsalvi2121@gmail.com>
2025-01-08 17:16:22 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "27019361"
+api_hash = "b75e05daee8ff985a85778dc203dbe30"
+bot_token = "8166245622:AAEHqSsfv-vavE6KkuJpS1A13ybZyqtv_wU"
